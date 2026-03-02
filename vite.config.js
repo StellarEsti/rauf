988492cc8cc513d682da7bec2291f28808392143
StellarEsti <wspyzmb666@gmail.com>
2026-03-02 21:12:18 +0800

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
       ],
     }),
   ],
-  base:'/academic-project-page-template-vue',
+  base:'/rauf',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
